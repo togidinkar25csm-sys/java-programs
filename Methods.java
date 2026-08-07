708090c5d2e0ae4daed3aa62d00fea900a0a05b6
togidinkar25csm-sys <togidinkar.25.csm@anits.edu.in>
2026-08-07 10:21:05 +0530

    Add files via upload

---
+++ Methods.java
@@ -0,0 +1,23 @@
+package myprog;
+
+public class Methods {
+void sample() {
+	
+System.out.println("this is a test method");
+}	
+void Example() {
+	System.out.println("this a another method");
+}
+	    public static void main(String[] args) {
+int x=10,y=20;
+int c=x+y;
+
+System.out.println("the result is" +c);
+  Methods bt = new Methods();
+  System.out.println("variable :" +bt);
+   bt.sample();
+   bt.Example();
+}
+}
+
+
